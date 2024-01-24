49f7bc3abb72e1ef6311730b743048b0031f4528
Taiki Ono <taiki@finatext.com>
2024-01-24 20:05:35 +0900

    Add slack retry config
    Signed-off-by: Taiki Ono <taiki@finatext.com>

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"time"
 
+	"github.com/Finatext/belldog/slack"
 	"github.com/aws/aws-lambda-go/lambda"
 	awsconfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
@@ -19,18 +21,27 @@ type Config struct {
 }
 
 // This doesn't follow go naming convention because it's used in envconfig.
+//
+// RetryReadTimeoutDuration: This will set to HTTP client's timeout.
+// Default HTTP client timeout covers from dialing (initiating TCP connection) to reading response body.
+// https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts
 type EnvConfig struct {
-	CustomDomainName                string     `split_words:"true"`
-	DdbTableName                    string     `split_words:"true" required:"true"`
-	LogLevel                        slog.Level `split_words:"true" default:"info"`
-	Mode                            string     `split_words:"true" required:"true"`
-	OpsNotificationChannelName      string     `split_words:"true" required:"true"`
-	ParameterNameSlackSigningSecret string     `split_words:"true" required:"true"`
-	ParameterNameSlackToken         string     `split_words:"true" required:"true"`
+	CustomDomainName                string        `split_words:"true"`
+	DdbTableName                    string        `split_words:"true" required:"true"`
+	LogLevel                        slog.Level    `split_words:"true" default:"info"`
+	Mode                            string        `split_words:"true" required:"true"`
+	OpsNotificationChannelName      string        `split_words:"true" required:"true"`
+	ParameterNameSlackSigningSecret string        `split_words:"true" required:"true"`
+	ParameterNameSlackToken         string        `split_words:"true" required:"true"`
+	RetryMax                        int           `split_words:"true" default:"3"`
+	RetryReadTimeoutDuration        time.Duration `split_words:"true" default:"5s"`
+	RetryWaitMaxDuration            time.Duration `split_words:"true" default:"10s"`
+	RetryWaitMinDuration            time.Duration `split_words:"true" default:"1s"`
 }
 
 var (
-	config Config
+	config           Config
+	slackRetryConfig slack.RetryConfig
 )
 
 func main() {
@@ -50,6 +61,7 @@ func main() {
 		os.Exit(1)
 	}
 	config.EnvConfig = c
+	slackRetryConfig = toSlackRetryConfig(config)
 
 	logLevel.Set(config.LogLevel)
 
@@ -99,4 +111,13 @@ func fetchParamter(ctx context.Context, paramName string) (string, error) {
 		return "", fmt.Errorf("empty SSM parameter value found: %s", paramName)
 	}
 	return value, nil
+}
+
+func toSlackRetryConfig(config Config) slack.RetryConfig {
+	return slack.RetryConfig{
+		Max:         config.RetryMax,
+		WaitMin:     config.RetryWaitMinDuration,
+		WaitMax:     config.RetryWaitMaxDuration,
+		ReadTimeout: config.RetryReadTimeoutDuration,
+	}
 }
