e2ad703996b70733195874b4ab5da81bd7bdaf5b
Taiki Ono <taiki@finatext.com>
2024-08-28 11:23:37 +0900

    Remove deprecated config
    Signed-off-by: Taiki Ono <taiki@finatext.com>

--- internal/appconfig/appconfig.go
+++ internal/appconfig/appconfig.go
@@ -11,19 +11,15 @@ import (
 // Default HTTP client timeout covers from dialing (initiating TCP connection) to reading response body.
 // https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts
 type Config struct {
-	CustomDomainName           string     `env:"CUSTOM_DOMAIN_NAME"`
-	DdbTableName               string     `env:"DDB_TABLE_NAME, required"`
-	GoLog                      slog.Level `env:"GO_LOG, default=info"`
-	Mode                       string     `env:"MODE, required"`
-	OpsNotificationChannelName string     `env:"OPS_NOTIFICATION_CHANNEL_NAME, required"`
-	// For backward compatibility
-	ParameterNameSlackSigningSecret string `env:"PARAMETER_NAME_SLACK_SIGNING_SECRET"`
-	SlackSigningSecret              string `env:"SLACK_SIGNING_SECRET"`
-	// For backward compatibility
-	ParameterNameSlackToken  string        `env:"PARAMETER_NAME_SLACK_TOKEN"`
-	SlackToken               string        `env:"SLACK_TOKEN"`
-	RetryMax                 int           `env:"RETRY_MAX, default=3"`
-	RetryReadTimeoutDuration time.Duration `env:"RETRY_READ_TIMEOUT_DURATION, default=5s"`
-	RetryWaitMaxDuration     time.Duration `env:"RETRY_WAIT_MAX_DURATION, default=10s"`
-	RetryWaitMinDuration     time.Duration `env:"RETRY_WAIT_MIN_DURATION, default=1s"`
+	CustomDomainName           string        `env:"CUSTOM_DOMAIN_NAME"`
+	DdbTableName               string        `env:"DDB_TABLE_NAME, required"`
+	GoLog                      slog.Level    `env:"GO_LOG, default=info"`
+	Mode                       string        `env:"MODE, required"`
+	OpsNotificationChannelName string        `env:"OPS_NOTIFICATION_CHANNEL_NAME, required"`
+	SlackSigningSecret         string        `env:"SLACK_SIGNING_SECRET"`
+	SlackToken                 string        `env:"SLACK_TOKEN"`
+	RetryMax                   int           `env:"RETRY_MAX, default=3"`
+	RetryReadTimeoutDuration   time.Duration `env:"RETRY_READ_TIMEOUT_DURATION, default=5s"`
+	RetryWaitMaxDuration       time.Duration `env:"RETRY_WAIT_MAX_DURATION, default=10s"`
+	RetryWaitMinDuration       time.Duration `env:"RETRY_WAIT_MIN_DURATION, default=1s"`
 }
